8a32cce69cbe2eca2709a017c8064b4688680163
startandroid <startofandroid@gmail.com>
2017-09-20 01:01:58 +0300

    Add FAB to word list screen

--- Vocabulary/app/src/main/java/ru/startandroid/vocabulary/ui/dictionary/list/mvp/WordListFragment.kt
+++ Vocabulary/app/src/main/java/ru/startandroid/vocabulary/ui/dictionary/list/mvp/WordListFragment.kt
@@ -1,18 +1,27 @@
 package ru.startandroid.vocabulary.ui.dictionary.list.mvp
 
 import android.os.Bundle
+import android.widget.Toast
+import butterknife.OnClick
 import ru.startandroid.vocabulary.R
 import ru.startandroid.vocabulary.base.ui.BaseFragment
 
 class WordListFragment : BaseFragment<WordListContract.Presenter>(), WordListContract.View {
     override fun getLayoutId() = R.layout.fragment_word_list
 
+    //@BindView(R.id.add)
+    //lateinit var floatingActionButtonAdd : FloatingActionButton
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         presenter.attachView(this);
         presenter.viewIsReady();
 
+    }
+
+    @OnClick(R.id.add)
+    fun onAddClick() {
+        Toast.makeText(activity, "click", Toast.LENGTH_LONG).show();
     }
 
 }
